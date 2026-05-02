beb6f91c2368656c8ee064b9b35b89ec6589d637
penguin-oo <3082639717@qq.com>
2026-05-02 18:39:59 +0800

    chore: remove remote openssh helper from repo

--- enable-remote-openssh-and-export-debug.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-set "SCRIPT=%~dp0enable-remote-openssh-and-export-debug.ps1"
-powershell -NoProfile -ExecutionPolicy Bypass -Command "Start-Process powershell -Verb RunAs -Wait -ArgumentList '-NoProfile','-ExecutionPolicy','Bypass','-File','''%SCRIPT%'''"
-endlocal
